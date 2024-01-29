7c4cb689f935207df1b8b43e72520ad00a98197c
Sushil Patil <129952964+sushilvpatil@users.noreply.github.com>
2024-01-29 11:11:10 +0530

    Add files via upload

---
+++ online parking System/src/online/parking/system/OnlineParkingSystem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package online.parking.system;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class OnlineParkingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s1=new Scanner(System.in);
+        
+        ArrayList i=new ArrayList();
+        i.add(1);
+        i.add(2);
+        i.add(3);
+        
+        
+        
+    }
+    
+}
+
+
+ 
